6f236b84709a28ec15989f4d690f7d9dc8fbbe27
Theara Kim <45727527+Thearakim@users.noreply.github.com>
2019-07-25 21:48:02 +0700

    Update int_to_bin.py

--- int_to_bin.py
+++ int_to_bin.py
@@ -1,10 +1,10 @@
-def int_to_binary(a, b):
-    sum = a + b
-    temBi = ""
-    while sum != 0:
-        temBi = str(sum % 2) + temBi
-        sum  //=  2
-    return temBi
+def int_to_binary(a, b): #function take two paramaters
+    sum = a + b    #sum two number take from print
+    temBi = ""     #declare string "" for adding the code below
+    while sum != 0:  #using loop to make sure a and b is greater than 0 because equal 0 error 
+        temBi = str(sum % 2) + temBi  #first it calculated sum % 2 take remainder to temBi 
+        sum  //=  2  #divided sum by 2 example 4 divided by 2 = 2 so loop again and temp add again 
+    return temBi #return binary value to our function int_to_binary
 
 
-print(int_to_binary(12, 3))
+print(int_to_binary(12, 3))  #put number a =12 and b =3 and print binary value
